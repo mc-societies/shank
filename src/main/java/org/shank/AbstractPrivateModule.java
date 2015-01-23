3590810e82ec24c58c2d6eba46e8d240e82014a0
p000ison <maximilian.ammann@googlemail.com>
2015-01-23 21:34:07 +0100

    Add private modules

---
+++ src/main/java/org/shank/AbstractPrivateModule.java
@@ -0,0 +1,37 @@
+package org.shank;
+
+import com.google.inject.PrivateModule;
+import com.google.inject.TypeLiteral;
+import com.google.inject.binder.LinkedBindingBuilder;
+import com.google.inject.name.Names;
+import net.catharos.lib.core.util.CastSafe;
+
+/**
+ * Represents a AbstractPrivateModule
+ */
+public abstract class AbstractPrivateModule extends PrivateModule {
+    protected <T> LinkedBindingBuilder<T> bindNamed(String name, Class<T> clazz) {
+        return bindNamed(name, TypeLiteral.get(clazz));
+    }
+
+    protected <T> LinkedBindingBuilder<T> bindNamed(String name, TypeLiteral<T> literal) {
+        return binder().bind(literal).annotatedWith(Names.named(name));
+    }
+
+    protected <T> void bindNamedInstance(String name, T instance) {
+        Class<T> type = CastSafe.toGeneric(instance.getClass());
+        bindNamedInstance(name, type, instance);
+    }
+
+    protected <T> void bindNamedInstance(String name, Class<T> clazz, T instance) {
+        bindNamed(name, clazz).toInstance(instance);
+    }
+
+    protected <T> void bindNamedInstance(String name, TypeLiteral<T> literal, T instance) {
+        bindNamed(name, literal).toInstance(instance);
+    }
+
+    protected void bindNamedString(String name, String string) {
+        bindNamedInstance(name, String.class, string);
+    }
+}
